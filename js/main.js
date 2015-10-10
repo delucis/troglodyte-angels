3ac81385407287a14180efa28ae8b457e3e318d8
delucis <swithinbank@gmail.com>
2015-10-10 18:32:43 -0400

    Add master gain node and link to range slider UI

--- js/main.js
+++ js/main.js
@@ -1,19 +1,26 @@
 var playButton = document.querySelector('#playNext');
 var stopButton = document.querySelector('#stopButton');
+var gainSlider = document.querySelector('#gainSlider');
+var gainSliderVal = gainSlider.value;
 var audioContext = new AudioContext();
-var gainNode = audioContext.createGain();
+var masterGainNode = audioContext.createGain();
+var masterGainVal = gainSliderVal;
+var oscGain = audioContext.createGain();
 var oscillator = audioContext.createOscillator();
 var oscillatorState = 0;
 
 oscillator.type = 'sine';
 oscillator.frequency.value = 1000;
-gainNode.gain.value = 0;
+oscGain.gain.value = 0;
+masterGainNode.gain.value = masterGainVal;
 
-oscillator.connect(gainNode);
-gainNode.connect(audioContext.destination);
+oscillator.connect(oscGain);
+oscGain.connect(masterGainNode);
+masterGainNode.connect(audioContext.destination);
 
 playButton.onmousedown = playCurve;
 stopButton.onmousedown = mute;
+gainSlider.oninput = adjustGain;
 
 function playCurve() {
   if (oscillatorState === 0) {
@@ -33,4 +40,10 @@ function mute() {
     oscGain.gain.linearRampToValueAtTime(0, audioContext.currentTime + 0.05);
     stopButton.disabled = true;
   }
+}
+
+function adjustGain() {
+  gainSliderVal = gainSlider.value;
+  masterGainNode.gain.linearRampToValueAtTime(gainSliderVal, audioContext.currentTime + 0.1);
+  masterGainVal = gainSliderVal;
 }
