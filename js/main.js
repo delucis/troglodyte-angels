924ec53b84c3b135f5a93f1ed765fd78b69d58a2
delucis <swithinbank@gmail.com>
2015-10-10 19:20:35 -0400

    Implement next cue increment/decrement mechanism

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 var playButton = document.querySelector('#playNext');
 var stopButton = document.querySelector('#stopButton');
 var gainSlider = document.querySelector('#gainSlider');
+var nextCueNum = document.querySelector('#nextCue');
+var incCueButton = document.querySelector('#incrementNextCue');
+var decCueButton = document.querySelector('#decrementNextCue');
 var gainSliderVal = gainSlider.value;
 var audioContext = new AudioContext();
 var masterGainNode = audioContext.createGain();
@@ -8,6 +11,10 @@ var masterGainVal = gainSliderVal;
 var oscGain = audioContext.createGain();
 var oscillator = audioContext.createOscillator();
 var oscillatorState = 0;
+
+var nextCue = nextCueNum.valueAsNumber;
+var minCue = 1;
+var maxCue = Infinity;
 
 oscillator.type = 'sine';
 oscillator.frequency.value = 1000;
@@ -20,7 +27,10 @@ masterGainNode.connect(audioContext.destination);
 
 playButton.onmousedown = playCurve;
 stopButton.onmousedown = mute;
+incCueButton.onmousedown = cueIncrement;
+decCueButton.onmousedown = cueDecrement;
 gainSlider.oninput = adjustGain;
+
 
 function playCurve() {
   if (oscillatorState === 0) {
@@ -39,6 +49,19 @@ function mute() {
     oscGain.gain.setValueAtTime(1, audioContext.currentTime);
     oscGain.gain.linearRampToValueAtTime(0, audioContext.currentTime + 0.05);
     stopButton.disabled = true;
+  }
+}
+
+function cueIncrement() {
+  if (nextCue < maxCue) {
+    nextCueNum.stepUp(1);
+    nextCue = nextCueNum.valueAsNumber;
+  }
+}
+function cueDecrement() {
+  if (nextCue > minCue) {
+    nextCueNum.stepDown(1);
+    nextCue = nextCueNum.valueAsNumber;
   }
 }
 
