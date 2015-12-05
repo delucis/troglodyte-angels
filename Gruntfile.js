40168f4ea3174f8559bbf751c40d4b5b20f38c08
delucis <swithinbank@gmail.com>
2015-12-05 13:51:16 -0500

    Get Grunt running for YAML —> JSON processing

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+  // 1. All configuration goes here
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    yaml: {
+      your_target: {
+        options: {
+          ignored: /^_/,
+          space: 2,
+          customTypes: {
+            '!include scalar': function(value, yamlLoader) {
+              return yamlLoader(value);
+            },
+            '!max sequence': function(values) {
+              return Math.max.apply(null, values);
+            },
+            '!extend mapping': function(value, yamlLoader) {
+              return _.extend(yamlLoader(value.basePath), value.partial);
+            }
+          }
+        },
+        files: [
+          {expand: true, cwd: '', src: ['*.yml'], dest: ''}
+        ]
+      }
+    },
+    'json-minify': {
+      build: {
+        files: 'cue-data.json'
+      }
+    },
+    watch: {
+      cues: {
+        files: ['cue-data.yml'],
+        tasks: ['yaml', 'json-minify']
+      }
+    }
+  });
+  require('load-grunt-tasks')(grunt);
+  grunt.registerTask('default', ['yaml', 'json-minify', 'watch']);
+};
